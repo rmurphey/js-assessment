1762c862c0d111966a482a53133bf972d898f98b
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2012-05-01 22:19:37 +0200

    More array tests

--- tests/app/arrays.js
+++ tests/app/arrays.js
@@ -36,6 +36,12 @@ define([ 'use!underscore' ], function(_) {
       expect(result[result.length - 1]).to.be(10);
     });
 
+    it("you should be able to remove the last item of an array", function() {
+      var result = fn(a);
+      expect(result).to.have.length(3);
+      expect(result.join(' ')).to.be('1 2 3');
+    });
+
     it("you should be able to join together two arrays", function() {
       // define a function for fn so that the following will pass
       var c = [ 'a', 'b', 'c', 1 ],
@@ -51,6 +57,29 @@ define([ 'use!underscore' ], function(_) {
 
       expect(result).to.have.length(5);
       expect(result.join(' ')).to.be('1 2 z 3 4');
+    });
+
+    it("you should be able to count the occurences of an item in an array", function() {
+      // define a function for fn so that the following will pass
+      var result = fn([ 1, 1, 1, 2, 1, 3 ], 1);
+
+      expect(result).to.be(4);
+    });
+
+    it("you should be able to find duplicates in an array", function() {
+      // define a function for fn so that the following will pass
+      var result = fn([ 1, 2, 4, 4, 3, 3, 1, 5 ]);
+
+      expect(result).to.have.length(3);
+      expect(result.sort().join(' ')).to.be('1 3 4');
+    });
+
+    it("you should be able to square each number in an array", function() {
+      // define a function for fn so that the following will pass
+      var result = fn(a);
+
+      expect(result).to.have.length(4);
+      expect(result.join(' ')).to.be('1 4 9 16');
     });
   });
 });
