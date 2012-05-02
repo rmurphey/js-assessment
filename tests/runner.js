ba667dc6c657a707fd0e5ca82ccc5b61595baef0
Loïc Kreitmann <loickreitmann@gmail.com>
2012-05-02 09:59:05 -0700

    Added flowControl and modules to runner.js

--- tests/runner.js
+++ tests/runner.js
@@ -2,9 +2,11 @@ var tests = [
   // link to test files here
 
   'tests/app/arrays',
-  'tests/app/objects',
-  'tests/app/functions',
   'tests/app/async',
+  'tests/app/flowControl',
+  'tests/app/functions',
+  'tests/app/modules',
+  'tests/app/objects',
   'tests/app/views'
 ];
 
