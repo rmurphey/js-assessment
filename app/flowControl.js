a6e458623d6e3579bd77df254c99784df505dac8
Rebecca Murphey <rmurphey@gmail.com>
2012-05-04 14:20:36 -0400

    separating flow control answers

---
+++ app/flowControl.js
@@ -0,0 +1,20 @@
+define([ 'use!underscore' ], function(_) {
+  return {
+    fizzBuzz : function(num) {
+      // write a function that receives a number as its argument;
+      // if the number is divisible by 3, the function should return 'fizz';
+      // if the number is divisible by 5, the function should return 'buzz';
+      // if the number is divisible the 3 and 5, the function should return
+      // 'fizzbuzz';
+      // otherwise the function should return the number
+    },
+
+    or : function(a, b) {
+
+    },
+
+    and : function(a, b) {
+
+    }
+  };
+});
