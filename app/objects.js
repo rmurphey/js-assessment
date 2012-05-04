ef3da38da3eace081d46409d4bf9d9e7ba07980a
Rebecca Murphey <rmurphey@gmail.com>
2012-05-04 15:29:55 -0400

    separating objects answers

---
+++ app/objects.js
@@ -0,0 +1,17 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define(function() {
+  return {
+    alterContext : function(fn, obj) {
+
+    },
+
+    alterObjects : function(constructor) {
+
+    },
+
+    iterate : function(obj) {
+
+    }
+  };
+});
