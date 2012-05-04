b42191563c4091c2b7c88211a013f33f614a9c56
Rebecca Murphey <rmurphey@gmail.com>
2012-05-04 15:12:10 -0400

    separate functions answers

---
+++ app/functions.js
@@ -0,0 +1,37 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define(function() {
+  return {
+    argsAsArray : function(arr) {
+
+    },
+
+    speak : function(fn, obj) {
+
+    },
+
+    functionFunction : function() {
+
+    },
+
+    partial : function() {
+
+    },
+
+    useArguments : function() {
+
+    },
+
+    callIt : function() {
+
+    },
+
+    curryIt : function() {
+
+    },
+
+    makeClosures : function() {
+
+    }
+  };
+});
