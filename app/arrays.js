847400639aae9e2037d73cb39cf74764f871e3dd
Rebecca Murphey <rmurphey@gmail.com>
2012-05-04 14:01:07 -0400

    separate arrays answers

---
+++ app/arrays.js
@@ -0,0 +1,45 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define([ 'use!underscore' ], function(_) {
+  return {
+    indexOf : function(arr, item) {
+
+    },
+
+    sum : function(arr) {
+
+    },
+
+    remove : function(arr, item) {
+
+    },
+
+    append : function(arr, item) {
+
+    },
+
+    truncate : function(arr) {
+
+    },
+
+    concat : function(arr1, arr2) {
+
+    },
+
+    insert : function(arr, item, index) {
+
+    },
+
+    count : function(arr, item) {
+
+    },
+
+    duplicates : function(arr) {
+
+    },
+
+    square : function(arr) {
+
+    }
+  };
+});
