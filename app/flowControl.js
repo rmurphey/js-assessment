c37b1f2f6f038d2d612498636b44f137ec210e48
Rebecca Murphey <rmurphey@gmail.com>
2012-08-26 22:40:45 -0400

    fix #48

--- app/flowControl.js
+++ app/flowControl.js
@@ -6,7 +6,7 @@ define(function() {
       // write a function that receives a number as its argument;
       // if the number is divisible by 3, the function should return 'fizz';
       // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible the 3 and 5, the function should return
+      // if the number is divisible by 3 and 5, the function should return
       // 'fizzbuzz';
       //
       // otherwise the function should return the number, or false if no number
