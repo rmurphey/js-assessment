47e7dbf30746efad70f0f7dac83170cd44d7249e
Timo Tijhof <krinklemail@gmail.com>
2013-01-03 07:08:03 +0100

    array: Add documentation, stricter tests.
    Add some documentation to clarify the intended return value.
    It isn't always obvious that the return value should be the
    array. Given that Array#push(), for example, returns the length.
    
    And stricter tests to make sure a copy is returned when expected,
    and in other case the original was affected.

--- app/arrays.js
+++ app/arrays.js
@@ -2,50 +2,108 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
+
+    /**
+     * @param {Array} arr
+     * @param {mixed} item
+     * @return number Index of item in arr.
+     */
     indexOf : function(arr, item) {
 
     },
 
+    /**
+     * @param {Array} arr Contains numerical values.
+     * @return number Sum of all values.
+     */
     sum : function(arr) {
 
     },
 
+    /**
+     * Remove all occurences of item from the array.
+     *
+     * @param {Array} arr The array to be changed.
+     * @param {mixed} item The value to be removed.
+     * @return {Array} arr
+     */
     remove : function(arr, item) {
 
     },
-    
+
+    /**
+     * Create a copy of the array without "item".
+     *
+     * @param {Array} arr The array to be changed.
+     * @param {mixed} item The value to be removed.
+     * @return {Array} copy
+     */
     removeWithCopy : function(arr, item) {
 
     },
 
+    /**
+     * @param {Array} arr
+     * @param {mixed} item
+     * @return {Array} arr
+     */
     append : function(arr, item) {
 
     },
 
+    /**
+     * @param {Array} arr
+     * @return {Array} arr
+     */
     truncate : function(arr) {
 
     },
 
-    concat : function(arr1, arr2) {
+    /**
+     * @param {Array} target
+     * @param {Array} origin Values to add to target
+     * @param {Array} target
+     */
+    concat : function(target, origin) {
 
     },
 
+    /**
+     * @param {Array} arr
+     * @param {mixed} item
+     * @param {number} index
+     */
     insert : function(arr, item, index) {
 
     },
 
+    /**
+     * @param {Array} arr
+     * @param {mixed} item
+     * @return {number}
+     */
     count : function(arr, item) {
 
     },
 
+    /**
+     * @param {Array} arr
+     */
     duplicates : function(arr) {
 
     },
 
+    /**
+     * @param {Array} arr
+     */
     square : function(arr) {
 
     },
 
+    /**
+     * @param {Array} arr
+     * @param {mixed} target
+     */
     findAllOccurrences : function(arr, target) {
 
     }
