1fe61ef8f0c4a68e8b89f7ab002b21ccab33a158
Lucas Myers <lucas@unthunk.com>
2013-08-26 09:37:41 -0700

    Added tests to logical operators
    Exisitng tests pass when bitwise operators are used

--- tests/app/logicalOperators.js
+++ tests/app/logicalOperators.js
@@ -11,12 +11,14 @@ define([
       expect(answers.and(false, false)).not.to.be.ok;
       expect(answers.and(true, false)).not.to.be.ok;
       expect(answers.and(true, true)).to.be.ok;
+      expect(answers.and(3, 4)).to.be.ok;
     });
 
     it('you should be able to work with logical or', function() {
       expect(answers.or(true, false)).to.be.ok;
       expect(answers.or(true, true)).to.be.ok;
       expect(answers.or(false, false)).not.to.be.ok;
+      expect(answers.or(3, 4)).to.not.eq(7);
     });
   });
 });
