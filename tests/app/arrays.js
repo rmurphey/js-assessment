4a236ca227ddbd2b3565bec0c995d5ab8bc90537
joholo <olof.johansson76@gmail.com>
2014-05-23 09:31:12 -0400

    Catch array count solution using [].reduce with missing initialvalue

--- tests/app/arrays.js
+++ tests/app/arrays.js
@@ -87,9 +87,9 @@ define([
     });
 
     it('you should be able to count the occurences of an item in an array', function() {
-      var result = answers.count([ 1, 1, 1, 2, 1, 3 ], 1);
+      var result = answers.count([ 1, 2, 4, 4, 3, 4, 3 ], 4);
 
-      expect(result).to.eql(4);
+      expect(result).to.eql(3);
     });
 
     it('you should be able to find duplicates in an array', function() {
