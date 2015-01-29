2df1d6c8e9e7e3b98b5b9421fcc3787742d97154
smurgolo <smurgolo@makingsense.com>
2015-01-29 16:16:18 -0300

    module test fixed.

--- app/modules.js
+++ app/modules.js
@@ -3,7 +3,13 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     createModule : function(str1, str2) {
-
+      return{
+        greeting: str1,
+        name: str2,
+        sayIt: function(){
+          return this.greeting + ', ' + this.name;
+        }
+      }
     }
   };
 });
