89538475c4e413727f82b925f1f07995a39be755
smurgolo <smurgolo@makingsense.com>
2015-01-29 16:10:39 -0300

    count category tests fixed

--- app/count.js
+++ app/count.js
@@ -1,8 +1,28 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(function () {
-  return {
+define(function () {  
+  return {    
     count : function (start, end) {
-    }
+      var continueCount = true;
+      var counter = start;
+      if(counter <= end) {
+        console.log(counter);
+        counter++;
+      }
+      var intervalFunc = function(){
+        if((counter <= end) && continueCount) {
+          console.log(counter);
+          counter++;
+        } else {
+            clearInterval(intervalFunc);
+        }
+      };
+      setInterval(intervalFunc, 100);          
+      return {
+        cancel: function(){
+          continueCount = false;
+          }
+        };
+    }    
   };
 });
