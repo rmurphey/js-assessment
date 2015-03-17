40792c28c95892a98e950529ef14be1b3009bb6f
Rob Simpson <rsimpson2@me.com>
2015-03-17 08:37:28 -0400

    Update flowControl.js
    The buzz and fizz where backwards for 3 and 5.

--- app/flowControl.js
+++ app/flowControl.js
@@ -4,8 +4,8 @@ define(function() {
   return {
     fizzBuzz : function(num) {
       // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
+      // if the number is divisible by 3, the function should return 'buzz';
+      // if the number is divisible by 5, the function should return 'fizz';
       // if the number is divisible by 3 and 5, the function should return
       // 'fizzbuzz';
       //
