c26ce62a7508978c7efcb57341243e1e696ceca5
Jacob M. Roufa <jroufa@quietlightcom.com>
2015-05-08 19:25:27 -0500

    Resolves rmurphey/js-assessment/issues/62
    Squashed commits in order to resolve rmurphey/js-assessment/pull/115
    
    Resolves rmurphey/js-assessment/issues/62
    
    Per @PAkerstrand's provided solution, this commit disallows a user
    from returning the result of a logical operator rather than the
    operation expression itself.
    
    Resolves rmurphey/js-assessment/issues/62
    
    This commit includes the assertion checking for use of bitwise operators
    which was previously left out of the PR commit.
    
    re-add assertion checking for bitwise operator

--- tests/app/logicalOperators.js
+++ tests/app/logicalOperators.js
@@ -8,16 +8,20 @@ define([
 ], function(answers) {
   describe('logical operators', function(){
     it('you should be able to work with logical or', function() {
+      expect(answers.or(false, true)).to.be.ok;
       expect(answers.or(true, false)).to.be.ok;
       expect(answers.or(true, true)).to.be.ok;
       expect(answers.or(false, false)).not.to.be.ok;
+      // the following assertion checks for bitwise operator
       expect(answers.or(3, 4)).to.not.eq(7);
     });
-      
+
     it('you should be able to work with logical and', function() {
-      expect(answers.and(false, false)).not.to.be.ok;
+      expect(answers.and(false, true)).not.to.be.ok;
       expect(answers.and(true, false)).not.to.be.ok;
       expect(answers.and(true, true)).to.be.ok;
+      expect(answers.and(false, false)).not.to.be.ok;
+      // the following assertion checks for bitwise operator
       expect(answers.and(3, 4)).to.be.ok;
     });
   });
