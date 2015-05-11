452435522c0351460ac191bd93b34d76a4b1a717
Ashley Williams <ashley@bocoup.com>
2015-05-11 17:30:22 -0400

    Remove require from tests
    - unwrap tests from define and closure
    - if in node, require answer file and chai expect
    - change 'answers' var to category specifc global, e.g. arraysAnswers
    - remove require from runner.html, add answers and tests files

--- tests/app/modules.js
+++ tests/app/modules.js
@@ -1,24 +1,21 @@
-/*globals describe:true, it:true, expect:true, beforeEach:true */
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-if (typeof expect !== 'function') { var expect = require('expect.js'); }
+if ( typeof window === 'undefined' ) {
+  require('../../app/modules');
+  var expect = require('chai').expect;
+}
 
-define([
-  'app/modules'
-], function(answers) {
-  describe('the module pattern', function() {
-    it('you should be able to create a function that returns a module', function() {
-      var module = answers.createModule('hello', 'matt');
+describe('the module pattern', function() {
+  it('you should be able to create a function that returns a module', function() {
+    var module = modulesAnswers.createModule('hello', 'matt');
 
-      expect(module.sayIt).to.be.a('function');
-      expect(module.name).to.eql('matt');
-      expect(module.greeting).to.eql('hello');
-      expect(module.sayIt()).to.eql('hello, matt');
+    expect(module.sayIt).to.be.a('function');
+    expect(module.name).to.eql('matt');
+    expect(module.greeting).to.eql('hello');
+    expect(module.sayIt()).to.eql('hello, matt');
 
-      module.name = 'katniss';
-      module.greeting = 'hi';
-      expect(module.name).to.eql('katniss');
-      expect(module.greeting).to.eql('hi');
-      expect(module.sayIt()).to.eql('hi, katniss');
-    });
+    module.name = 'katniss';
+    module.greeting = 'hi';
+    expect(module.name).to.eql('katniss');
+    expect(module.greeting).to.eql('hi');
+    expect(module.sayIt()).to.eql('hi, katniss');
   });
 });
--- tests/app/objects.js
+++ tests/app/objects.js
@@ -1,66 +1,63 @@
-/*globals describe:true, it:true, expect:true, beforeEach:true */
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-if (typeof expect !== 'function') { var expect = require('expect.js'); }
+if ( typeof window === 'undefined' ) {
+  require('../../app/objects');
+  var expect = require('chai').expect;
+}
 
-define([
-  'app/objects'
-], function(answers) {
-  describe('objects and context', function() {
-    var a;
-    var b;
-    var C;
+describe('objects and context', function() {
+  var a;
+  var b;
+  var C;
 
-    beforeEach(function() {
-      a = {
-        name : 'Matt',
-        greeting : 'Hello',
-        sayIt : function() {
-          return  this.greeting + ', ' +
-                  this.name + '!';
-        }
-      };
+  beforeEach(function() {
+    a = {
+      name : 'Matt',
+      greeting : 'Hello',
+      sayIt : function() {
+        return  this.greeting + ', ' +
+                this.name + '!';
+      }
+    };
 
-      b = {
-        name : 'Rebecca',
-        greeting : 'Yo'
-      };
+    b = {
+      name : 'Rebecca',
+      greeting : 'Yo'
+    };
 
-      C = function(name) {
-        this.name = name;
-        return this;
-      };
-    });
+    C = function(name) {
+      this.name = name;
+      return this;
+    };
+  });
 
-    it('you should be able to alter the context in which a method runs', function() {
+  it('you should be able to alter the context in which a method runs', function() {
       // define a function for fn so that the following will pass
-      expect(answers.alterContext(a.sayIt, b)).to.eql('Yo, Rebecca!');
-    });
+    expect(objectsAnswers.alterContext(a.sayIt, b)).to.eql('Yo, Rebecca!');
+  });
 
-    it('you should be able to alter multiple objects at once', function() {
-      // define a function for fn so that the following will pass
-      var obj1 = new C('Rebecca');
-      var obj2 = new C('Melissa');
-      var greeting = 'What\'s up';
+  it('you should be able to alter multiple objects at once', function() {
+    // define a function for fn so that the following will pass
+    var obj1 = new C('Rebecca');
+    var obj2 = new C('Melissa');
+    var greeting = 'What\'s up';
 
-      answers.alterObjects(C, greeting);
+    objectsAnswers.alterObjects(C, greeting);
 
-      expect(obj1.greeting).to.eql(greeting);
-      expect(obj2.greeting).to.eql(greeting);
-      expect(new C('Ellie').greeting).to.eql(greeting);
-    });
+    expect(obj1.greeting).to.eql(greeting);
+    expect(obj2.greeting).to.eql(greeting);
+    expect(new C('Ellie').greeting).to.eql(greeting);
+  });
 
-    it('you should be able to iterate over an object\'s "own" properties', function() {
-      // define a function for fn so that the following will pass
-      var C = function() {
-        this.foo = 'bar';
-        this.baz = 'bim';
-      };
+  it('you should be able to iterate over an object\'s "own" properties', function() {
+    // define a function for fn so that the following will pass
+    var C = function() {
+      this.foo = 'bar';
+      this.baz = 'bim';
+    };
 
-      C.prototype.bop = 'bip';
+    C.prototype.bop = 'bip';
 
-      var obj = new C();
+    var obj = new C();
 
-      expect(answers.iterate(obj)).to.eql([ 'foo: bar', 'baz: bim' ]);
-    });
+    expect(objectsAnswers.iterate(obj)).to.eql([ 'foo: bar', 'baz: bim' ]);
   });
 });
