8be84011c40f6dc96c39c822560829b9debd7873
Jacob M. Roufa <jroufa@quietlightcom.com>
2015-05-13 10:42:27 -0500

    remove answers for count -- accidentally committed

--- app/count.js
+++ app/count.js
@@ -2,21 +2,6 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
-    var timeout;
-    function doIt () {
-      console.log(start++);
 
-      if (start <= end) {
-        timeout = setTimeout(doIt, 100);
-      }
-    }
-
-    doIt();
-
-    return {
-      cancel : function () {
-        timeout && clearTimeout(timeout);
-      }
-    };
   }
 };
