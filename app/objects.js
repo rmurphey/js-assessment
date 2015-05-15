7ffc97fa2fdd761df16eca8ac220be8f0853f57f
Paul Grever <paulgrever@gmail.com>
2015-05-15 14:00:56 -0600

    completed two objects

--- app/objects.js
+++ app/objects.js
@@ -2,11 +2,11 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-  
+   return fn.apply(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
