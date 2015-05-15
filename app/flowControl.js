bec4814ed223bd6fdc05511f9028ace8a8cd22c2
Paul Grever <paulgrever@gmail.com>
2015-05-15 13:50:51 -0600

    did flow controL

--- app/flowControl.js
+++ app/flowControl.js
@@ -2,6 +2,19 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.flowControlAnswers = {
   fizzBuzz : function(num) {
+    if (isNaN(num)){
+        return false;
+    } else{
+        if ((num % 5 ===0 ) && (num % 3===0)){
+            return 'fizzbuzz';
+        } else if (num % 5===0) {
+            return 'buzz';
+        } else if (num % 3 === 0){
+            return 'fizz';
+        } else{
+            return num;
+        }
+    }
     // write a function that receives a number as its argument;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
