c3a26da9fd18e4e0633f24373823004a6ee7857b
Mike Gehard <mgehard@pivotal.io>
2015-05-31 15:43:50 -0700

    improve array duplicates test
    Remove duplicate assertion
    
    In the remaining assertion you can clearly see that there are
    three elements in the array.
    
    In my mind, removing this assertion removes a bit of duplication
    in the tests. This makes them a bit better than they were before.
    
    Futher simplify remaining assertion
    
    Removing the join() preserves the visibility of the return type
    of the function.
    
    [close #132]

--- tests/app/arrays.js
+++ tests/app/arrays.js
@@ -93,8 +93,7 @@ describe('arrays', function() {
   it('you should be able to find duplicates in an array', function() {
     var result = arraysAnswers.duplicates([ 1, 2, 4, 4, 3, 3, 1, 5, 3 ]);
 
-    expect(result).to.have.length(3);
-    expect(result.sort().join(' ')).to.eql('1 3 4');
+    expect(result.sort()).to.eql([1, 3, 4]);
   });
 
   it('you should be able to square each number in an array', function() {
