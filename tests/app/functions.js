dece6cfbab5cf42feaf015823b6dc54240c79a5e
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-06-29 16:06:10 -0400

    remove flowcontrol, add more function problems

--- tests/app/functions.js
+++ tests/app/functions.js
@@ -54,4 +54,28 @@ describe('functions', function() {
     expect(result2).to.eql(0);
   });
 
+  it('it returns \'fizz\' if input is divisible by 3, \'buzz\' if 5, \'fizzbuzz\' if 15, else the input', function() {
+    var num = 0;
+
+    while (num % 3 === 0 || num % 5 === 0) {
+      num = Math.floor(Math.random() * 10) + 1;
+    }
+
+    expect(functionAnswers.fizzBuzz()).not.to.be.ok;
+    expect(functionAnswers.fizzBuzz('foo')).not.to.be.ok;
+    expect(functionAnswers.fizzBuzz(2)).to.eql(2);
+    expect(functionAnswers.fizzBuzz(101)).to.eql(101);
+
+    expect(functionAnswers.fizzBuzz(3)).to.eql('fizz');
+    expect(functionAnswers.fizzBuzz(6)).to.eql('fizz');
+    expect(functionAnswers.fizzBuzz(num * 3)).to.eql('fizz');
+
+    expect(functionAnswers.fizzBuzz(5)).to.eql('buzz');
+    expect(functionAnswers.fizzBuzz(10)).to.eql('buzz');
+    expect(functionAnswers.fizzBuzz(num * 5)).to.eql('buzz');
+
+    expect(functionAnswers.fizzBuzz(15)).to.eql('fizzbuzz');
+    expect(functionAnswers.fizzBuzz(num * 3 * 5)).to.eql('fizzbuzz');
+  });
+
 });
