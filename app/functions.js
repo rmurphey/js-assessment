911bbdd6efa7dd10e25ddd61084d2e74f18aa07e
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-06-29 14:31:29 -0400

    add some function challenges

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,19 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray : function(fn, arr) {
+  reverseString : function(str) {
 
   },
 
-  speak : function(fn, obj) {
+  longestSubString : function(str) {
 
   },
 
-  functionFunction : function(str) {
+  letterMoveForward : function(str) {
 
   },
 
-  makeClosures : function(arr, fn) {
-
-  },
-
-  partial : function(fn, str1, str2) {
-
-  },
-
-  useArguments : function() {
-
-  },
-
-  callIt : function(fn) {
-
-  },
-
-  partialUsingArguments : function(fn) {
-
-  },
-
-  curryIt : function(fn) {
+  capitalizeWords : function(arr, fn) {
 
   }
 };
--- tests/app/functions.js
+++ tests/app/functions.js
@@ -4,142 +4,37 @@ if ( typeof window === 'undefined' ) {
 }
 
 describe('functions', function() {
-  var sayItCalled = false;
-  var sayIt = function(greeting, name, punctuation) {
-        sayItCalled = true;
-        return greeting + ', ' + name + (punctuation || '!');
-      };
+  var string;
 
   beforeEach(function () {
-    sayItCalled = false;
+    string = "launch academy!";
   });
 
-  it('you should be able to use an array as arguments when calling a function', function() {
-    var result = functionsAnswers.argsAsArray(sayIt, [ 'Hello', 'Ellie', '!' ]);
-    expect(result).to.eql('Hello, Ellie!');
-    expect(sayItCalled).to.be.ok;
+  it('should reverse a string', function(){
+    var result = functionAnswers.reverseString(string);
+
+    expect(result).to.eql('!ymedaca hcnual')
   });
 
-  it('you should be able to change the context in which a function is called', function() {
-    var speak = function() {
-          return sayIt(this.greeting, this.name, '!!!');
-        };
-    var obj = {
-          greeting : 'Hello',
-          name : 'Rebecca'
-        };
+  it('should be able to determine the longest substring in a string', function() {
+    var result = functionAnswers.longestSubString(string);
 
-    var result = functionsAnswers.speak(speak, obj);
-    expect(result).to.eql('Hello, Rebecca!!!');
-    expect(sayItCalled).to.be.ok;
+    expect(result).to.eql('academy!');
   });
 
-  it('you should be able to return a function from a function', function() {
-    expect(functionsAnswers.functionFunction('Hello')('world')).to.eql('Hello, world');
-    expect(functionsAnswers.functionFunction('Hai')('can i haz funxtion?')).to.eql('Hai, can i haz funxtion?');
+  it('should replace a string of letters with letters that follow them in the alphabet', function() {
+    var string1 = "abcdef";
+    var string2 = "z3bac";
+    var result1 = functionAnswers.letterMoveForward(string);
+    var result2 = functionAnswers.letterMoveForward(string2);
+
+    expect(result1).to.eql('bcdefg');
+    expect(result2).to.eql('a3cbd');
   });
 
-  it('you should be able to use closures', function () {
-    var arr = [ Math.random(), Math.random(), Math.random(), Math.random() ];
-    var square = function (x) { return x * x; };
-
-    var funcs = functionsAnswers.makeClosures(arr, square);
-    expect(funcs).to.have.length(arr.length);
-
-    for (var i = 0; i < arr.length; i++) {
-      expect(funcs[i]()).to.eql(square(arr[i]));
-    }
+  it('should capitalize the first letter of each word in a string', function() {
+    var result = functionAnswers.capitalizeWords(string);
+    expect(result).to.eql('Launch Academy!');
   });
 
-  it('you should be able to create a "partial" function', function() {
-    var partial = functionsAnswers.partial(sayIt, 'Hello', 'Ellie');
-    expect(partial('!!!')).to.eql('Hello, Ellie!!!');
-    expect(sayItCalled).to.be.ok;
-  });
-
-  it('you should be able to use arguments', function () {
-    var a = Math.random();
-    var b = Math.random();
-    var c = Math.random();
-    var d = Math.random();
-
-    expect(functionsAnswers.useArguments(a)).to.eql(a);
-    expect(functionsAnswers.useArguments(a, b)).to.eql(a + b);
-    expect(functionsAnswers.useArguments(a, b, c)).to.eql(a + b + c);
-    expect(functionsAnswers.useArguments(a, b, c, d)).to.eql(a + b + c + d);
-  });
-
-  it('you should be able to apply functions with arbitrary numbers of arguments', function () {
-    (function () {
-      var a = Math.random();
-      var b = Math.random();
-      var c = Math.random();
-
-      var wasITake2ArgumentsCalled = false;
-      var iTake2Arguments = function (firstArgument, secondArgument) {
-        expect(arguments.length).to.eql(2);
-        expect(firstArgument).to.eql(a);
-        expect(secondArgument).to.eql(b);
-
-        wasITake2ArgumentsCalled = true;
-      };
-
-      var wasITake3ArgumentsCalled = false;
-      var iTake3Arguments = function (firstArgument, secondArgument, thirdArgument) {
-        expect(arguments.length).to.eql(3);
-        expect(firstArgument).to.eql(a);
-        expect(secondArgument).to.eql(b);
-        expect(thirdArgument).to.eql(c);
-
-        wasITake3ArgumentsCalled = true;
-      };
-
-      functionsAnswers.callIt(iTake2Arguments, a, b);
-      functionsAnswers.callIt(iTake3Arguments, a, b, c);
-
-      expect(wasITake2ArgumentsCalled).to.be.ok;
-      expect(wasITake3ArgumentsCalled).to.be.ok;
-    }());
-  });
-
-  it('you should be able to create a "partial" function for variable number of applied arguments', function () {
-    var partialMe = function (x, y, z) {
-      return x / y * z;
-    };
-
-    var a = Math.random();
-    var b = Math.random();
-    var c = Math.random();
-    expect(functionsAnswers.partialUsingArguments(partialMe)(a, b, c)).to.eql(partialMe(a, b, c));
-    expect(functionsAnswers.partialUsingArguments(partialMe, a)(b, c)).to.eql(partialMe(a, b, c));
-    expect(functionsAnswers.partialUsingArguments(partialMe, a, b)(c)).to.eql(partialMe(a, b, c));
-    expect(functionsAnswers.partialUsingArguments(partialMe, a, b, c)()).to.eql(partialMe(a, b, c));
-  });
-
-  it('you should be able to curry existing functions', function () {
-    var curryMe = function (x, y, z) {
-      return x / y * z;
-    };
-
-    var a = Math.random();
-    var b = Math.random();
-    var c = Math.random();
-    var result;
-
-    result = functionsAnswers.curryIt(curryMe);
-    expect(typeof result).to.eql('function');
-    expect(result.length).to.eql(1);
-
-    result = functionsAnswers.curryIt(curryMe)(a);
-    expect(typeof result).to.eql('function');
-    expect(result.length).to.eql(1);
-
-    result = functionsAnswers.curryIt(curryMe)(a)(b);
-    expect(typeof result).to.eql('function');
-    expect(result.length).to.eql(1);
-
-    result = functionsAnswers.curryIt(curryMe)(a)(b)(c);
-    expect(typeof result).to.eql('number');
-    expect(result).to.eql(curryMe(a, b, c));
-  });
 });
