d8598cb440c41d76d28619d56d8abdb8856c7d76
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-07-15 13:43:57 -0400

    add evening exercise material

---
+++ app/eveningExercise.js
@@ -0,0 +1,35 @@
+exports = (typeof window === 'undefined') ? global : window;
+
+/**
+ * This file defines an object with some methods. Some of these methods are
+ * populated incorrectly; your job is to fix them. Other methods are not
+ * populated at all; your job is to fill them out.
+ */
+
+exports.eveningExerciseAnswers = {
+  globals : function() {
+    myObject = {
+      name : 'Jory'
+    };
+
+    return myObject;
+  },
+
+  functions : function(flag) {
+    if (flag) {
+      function getValue() { return 'a'; }
+    } else {
+      function getValue() { return 'b'; }
+    }
+
+    return getValue();
+  },
+
+  parseInt : function(num) {
+    return parseInt(num);
+  },
+
+  identity : function(val1, val2) {
+
+  }
+};
--- tests/app/functions.js
+++ tests/app/functions.js
@@ -4,38 +4,6 @@ if ( typeof window === 'undefined' ) {
 }
 
 describe('functions', function() {
-  var string;
-
-  beforeEach(function () {
-    string = "launch academy!";
-  });
-
-  it('should reverse a string', function(){
-    var result = functionAnswers.reverseString(string);
-
-    expect(result).to.eql('!ymedaca hcnual')
-  });
-
-  it('should be able to determine the longest substring in a string', function() {
-    var result = functionAnswers.longestSubString(string);
-
-    expect(result).to.eql('academy!');
-  });
-
-  it('should replace a string of letters with letters that follow them in the alphabet', function() {
-    var string1 = "abcdef";
-    var string2 = "z3bac";
-    var result1 = functionAnswers.letterMoveForward(string);
-    var result2 = functionAnswers.letterMoveForward(string2);
-
-    expect(result1).to.eql('bcdefg');
-    expect(result2).to.eql('a3cbd');
-  });
-
-  it('should capitalize the first letter of each word in a string', function() {
-    var result = functionAnswers.capitalizeWords(string);
-    expect(result).to.eql('Launch Academy!');
-  });
 
   it('should return true or false based on a number being prime', function() {
     var _true = functionAnswers.isPrime(5);
