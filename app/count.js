a7f2e0fbe095d86062b4263c575f438bcad49507
Vasil Vanchuk <v.vanchuk@tut.by>
2015-08-24 17:23:07 +0300

    Resolve counter implementation

--- app/count.js
+++ app/count.js
@@ -2,6 +2,25 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    'use strict';
+    var timeOut;
 
+    var iteration = function(i) {
+      console.log(i);
+
+      if(i < end) {
+        timeOut = setTimeout(iteration, 100, i+1);
+      }
+    };
+
+    if(i <= end) {
+      iteration(start);
+    }
+
+    return {
+      cancel: function() {
+        clearTimeout(timeOut);
+      }
+    }
   }
 };
