bd8918e49657de6539a670bc1105e5ddfb9757f6
Vasil Vanchuk <v.vanchuk@tut.by>
2015-08-25 13:11:42 +0300

    Fill regular expressions tests

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    return !!str.match(/\d/);
   },
 
   containsRepeatingLetter : function(str) {
-
+    return !!str.match(/([a-zA-Z])\1/);
   },
 
   endsWithVowel : function(str) {
-
+    return !!str.match(/[aeiou]$/i);
   },
 
   captureThreeNumbers : function(str) {
-
+    return (str.match(/\d{3}/) || [false]).pop();
   },
 
   matchesPattern : function(str) {
-
+    return !!str.match(/^\d{3}-\d{3}-\d{4}$/);
   },
   isUSD : function(str) {
-
+    var ret = !!str.match(/^\$([0-9]{1,3},([0-9]{3},)*[0-9]{3}|[0-9]+)(\.[0-9][0-9])?$/);
+    console.log(str, ret);
+    return ret;
   }
 };
