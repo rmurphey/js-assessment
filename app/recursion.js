87ef490b4b9d845220324af151dfb10bd86f4d43
Vasil Vanchuk <v.vanchuk@tut.by>
2015-08-25 12:00:08 +0300

    Fill recursion tests

--- app/recursion.js
+++ app/recursion.js
@@ -1,8 +1,20 @@
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
-  listFiles: function(data, dirName) {
+  listFiles: function listFiles(data, dirName) {
+    var fileList = [];
 
+    (data.files || []).forEach(function(item) {
+      if(typeof  item === 'string') {
+        if(!dirName || dirName === data.dir) {
+          fileList.push(item);
+        }
+      } else {
+        var dirFiles = listFiles(item, (!dirName || dirName === data.dir)? undefined: dirName);
+        fileList.push.apply(fileList, dirFiles);
+      }
+    });
+    return fileList;
   },
 
   permute: function(arr) {
