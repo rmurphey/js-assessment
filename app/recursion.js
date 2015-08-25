7b7c60a3d23bb7dd8a4b5a5e11418923483625af
Vasil Vanchuk <v.vanchuk@tut.by>
2015-08-25 12:49:21 +0300

    Fill permutations tests

--- app/recursion.js
+++ app/recursion.js
@@ -17,15 +17,50 @@ exports.recursionAnswers = {
     return fileList;
   },
 
-  permute: function(arr) {
-
+  permute: function(arr) { // http://stackoverflow.com/questions/9960908/permutations-in-javascript/11509565#11509565
+    var permArr = [],
+      usedChars = [];
+    return (function main() {
+      for (var i = 0; i < arr.length; i++) {
+        var ch = arr.splice(i, 1)[0];
+        usedChars.push(ch);
+        if (arr.length == 0) {
+          permArr.push(usedChars.slice());
+        }
+        main();
+        arr.splice(i, 0, ch);
+        usedChars.pop();
+      }
+      return permArr;
+    })();
   },
 
-  fibonacci: function(n) {
+  fibonacci: function fibonacci(n) {
+    if(!fibonacci.cache) {
+      fibonacci.cache = {};
+    }
+    if(!fibonacci.cache[n]) {
+      if(n<=2) {
+        fibonacci.cache[n] = 1;
+      } else {
+        fibonacci.cache[n] = fibonacci(n -1) + fibonacci(n -2);
+      }
+    }
 
+    return fibonacci.cache[n];
   },
 
-  validParentheses: function(n) {
-
+  validParentheses: function validParentheses(n) {
+    if(n === 1) {
+      return ['()'];
+    }
+    var prevParentheses = validParentheses(n-1);
+    var list = {};
+    prevParentheses.forEach(function(item) {
+      list['(' + item + ')'] = null;
+      list['()' + item] = null;
+      list[item + '()'] = null;
+    });
+    return Object.keys(list);
   }
 };
