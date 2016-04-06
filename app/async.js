7ff904e48c0b2d62c150c80b57e2e965700d7c3f
rimmesbe <ryanimmesberger@gmail.com>
2016-04-06 13:28:54 -0400

    got async.js code passing tests

--- app/async.js
+++ app/async.js
@@ -2,10 +2,34 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var promise = new Promise(function(resolve, reject) {
+      if(value){
+        resolve(value);
+      } else {
+        reject(Error("No value"));
+      }
+    });
+    return promise;
   },
 
   manipulateRemoteData : function(url) {
+    var parseJson = function(response) {
+      return response.json();
+    };
 
+    var getSortedValues = function(data){
+      var parsedAry = [];
+      for(var key in data) {
+        if(typeof data[key] == 'object') {
+          parsedAry = parsedAry.concat(getSortedValues(data[key]));
+        } else {
+          parsedAry.push(data[key]);
+        }
+      }
+      return parsedAry.sort();
+    };
+
+    var promise = fetch(url).then(parseJson);
+    return promise.then(getSortedValues);
   }
 };
