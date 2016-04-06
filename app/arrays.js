2e26837721d9f30ac3b80dab91d9c4aacfd11e04
rimmesbe <ryanimmesberger@gmail.com>
2016-04-06 13:41:34 -0400

    got array.js indexOf() test passing

--- app/arrays.js
+++ app/arrays.js
@@ -3,7 +3,10 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    for(var i=0; i<arr.length; i++){
+      if(arr[i] == item){return i;}
+    }
+    return -1;
   },
 
   sum : function(arr) {
