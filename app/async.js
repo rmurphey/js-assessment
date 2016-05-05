f707362a8ef25e6c597c47079829da34f8c118dc
ashwin <ashwin.usa@gmail.com>
2016-05-05 14:44:28 -0400

    js assesment test

--- app/async.js
+++ app/async.js
@@ -2,10 +2,27 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+    var deferred = $.Deferred();
+       setTimeout(function() {
+         deferred.resolve(value);
+       }, 1);
+    return deferred.promise();
 
   },
 
   manipulateRemoteData: function(url) {
 
+  var deferred = $.Deferred();
+
+      $.ajax(url).then(function(resp) {
+        var per = $.map(resp.people, function(person) {
+          return person.name;
+        });
+        deferred.resolve(per.sort());
+      });
+
+      return deferred.promise();
   }
+
+
 };
--- app/flowControl.js
+++ app/flowControl.js
@@ -3,13 +3,33 @@ exports = typeof window === 'undefined' ? global : window;
 exports.flowControlAnswers = {
   fizzBuzz: function(num) {
     // write a function that receives a number as its argument;
-    // if the number is divisible by 3, the function should return 'fizz';
-    // if the number is divisible by 5, the function should return 'buzz';
-    // if the number is divisible by 3 and 5, the function should return
-    // 'fizzbuzz';
-    //
-    // otherwise the function should return the number, or false if no number
+
+     //return false if no number
     // was provided or the value provided is not a number
+
+    if (typeof num !== 'number') {
+	      return false;
+    }
+
+
+ // if the number is divisible by 3 and 5, the function should return
+	    // 'fizzbuzz';
+	    if (num % 3 === 0 && num % 5 === 0) {
+		      return 'fizzbuzz';
+    }
+
+     // if the number is divisible by 3, the function should return 'fizz';
+    if (num % 3 === 0) {
+	      return 'fizz';
+	    }
+
+  // if the number is divisible by 5, the function should return 'buzz';
+	    if (num % 5 === 0) {
+	      return 'buzz';
+    }
+    //
+    // otherwise the function should return the number, o
+    return num;
 
   }
 };
