feaa04d9253b1d91a01eda62ece73aaddad4c68a
Phoebe <Phoebe>
2016-10-06 19:15:46 -0400

    Initial commit and code changes for objects and async

--- app/recursion.js
+++ app/recursion.js
@@ -2,11 +2,37 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+var listOfFiles = [];
+var dirs = [];
+
+function processDir(dir)  {
+var i;
+var len;
+var file;
+var files = dir.files;
+
+dirs.push(dir,dir);
+for(i=0, len = files.length; i < len; i++) {
+file = files[i];
+if (typeof file === 'string')  {
+if (!dirName || dirs.indexOf (dirName)  > -1) {
+listOfFiles.push(files[i]);
+}
+}else {
+processDir(files[i]);
+}
+}
+dirs.pop();
+}
+processDir(data);
+return listOfFiles;
 
   },
 
-  permute: function(arr) {
 
+     
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         );
+     
   },
 
   fibonacci: function(n) {
