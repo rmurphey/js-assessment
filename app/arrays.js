528ddbee813dfda3063541837127cd119afc11cc
johnlyden <johnlydennc@gmail.com>
2016-12-17 22:47:02 -0500

    adding solutions up until removing first item of an array

--- app/arrays.js
+++ app/arrays.js
@@ -37,15 +37,18 @@ exports.arraysAnswers = {
     },
 
     append: function(arr, item) {
-
+        arr.push(item);
+        return arr
     },
 
     truncate: function(arr) {
-
+        arr.pop();
+        return arr;
     },
 
     prepend: function(arr, item) {
-
+        arr.unshift(item);
+        return arr;
     },
 
     curtail: function(arr) {
