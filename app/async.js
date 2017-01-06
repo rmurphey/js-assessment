abb7f7cc599bacce87bf5f9cd0c387450fc3b2e8
johnlyden <johnlydennc@gmail.com>
2017-01-05 19:33:22 -0500

    adding solution for sorted array promise async problem

--- app/async.js
+++ app/async.js
@@ -1,36 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-    return new Promise(function(resolve, reject){
-      resolve(value);
-    })
-  },
+    async: function(value) {
+        return new Promise(function(resolve, reject){
+            resolve(value);
+        })
+    },
 
-  manipulateRemoteData: function(url) {
-    var promise = new Promise(function(resolve, reject){
-      resolve(function(){
-        return result;
-      });
-    });
+    manipulateRemoteData: function(url) {
+        return new Promise(function(resolve, reject){
+            $.ajax({
+                url: url
+            }).done(function(result){
+                var ary = $.map(result.people, function(v, i, a){
+                    return [v.name];
+                });
+                resolve(ary.sort());
+            });
 
-    var xhr = new XMLHttpRequest();
-
-    xhr.addEventListener("load", function(){
-
-      promise.then(function(){
-        let pplObject = this.responseText.people;
-        
-        return (pplObject.map(function(e, i, a){
-          return e.name;
-        })).sort();
-      });
-    });
-
-    xhr.open("GET",url);
-
-    xhr.send(null);
-
-    return promise;
-  }
+        });
+    }
 };
