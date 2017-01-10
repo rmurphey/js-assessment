f0f047a426dbe89d5fd6e1a54b915cc0c296220e
johnlyden <johnlydennc@gmail.com>
2017-01-09 22:16:07 -0500

    adding first two answers for functions section

--- app/functions.js
+++ app/functions.js
@@ -2,11 +2,11 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn.apply(this, arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
