3d9400b2825384cfc7a7bc6feefd8bb4383b6e10
johnlyden <johnlydennc@gmail.com>
2017-01-23 19:10:40 -0500

    adding regex solutions for first two problems

--- app/regex.js
+++ app/regex.js
@@ -2,11 +2,11 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+      return /\d/.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    return /([a-zA-Z]).*?\1/.test(str);
   },
 
   endsWithVowel: function(str) {
