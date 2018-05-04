fb3f8f66b9213423e35708caeb683abef42ec60c
David Ryan <djrdev@icloud.com>
2018-05-03 20:00:14 -0600

    truncate & prepend

--- app/arrays.js
+++ app/arrays.js
@@ -6,27 +6,41 @@ exports.arraysAnswers = {
   },
 
   sum: function(arr) {
+    return arr.reduce((accu, ele) => {
+      accu += ele;
 
+      return accu;
+    }, 0)
   },
 
   remove: function(arr, item) {
-
+    return arr.filter(ele => ele !== item)
   },
 
   removeWithoutCopy: function(arr, item) {
 
+    return arr.filter(ele => ele === item)
   },
 
   append: function(arr, item) {
+    const newArray = arr;
+    newArray.push(item);
 
+    return newArray;
   },
 
   truncate: function(arr) {
+    const newArray = arr;
+    newArray.pop();
 
+    return newArray;
   },
 
   prepend: function(arr, item) {
+    const newArray = arr;
+    newArray.unshift(item);
 
+    return newArray;
   },
 
   curtail: function(arr) {
