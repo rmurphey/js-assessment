27b6267e6e45ccc0bf5d3871012a450d6ac13b6e
Cristopher Uriel <theledzed@gmail.com>
2018-12-14 17:32:07 -0600

    Array remove, sum, indexOf

--- app/arrays.js
+++ app/arrays.js
@@ -2,15 +2,34 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+    for ( var i = 0, j = arr.length; i < j; i++) {
+      if (arr[i] === item){
+        return i;
+      }
+    }
 
+    return -1;
   },
 
   sum: function(arr) {
+    var suma = 0;
 
+    for (var i = 0, j = arr.length; i < j; i++){
+      suma += arr[i];
+    }
+    return suma;
   },
 
   remove: function(arr, item) {
 
+    var remover = [];
+
+    for (var i = 0, j = arr.length; i < j; i++){
+      if (arr[i] !== item){
+        remover.push(arr[i]);
+      }
+  }
+    return remover;
   },
 
   removeWithoutCopy: function(arr, item) {
