e32ae058e12147bcba1f0e31b1f4254875c7a42e
Cristopher Uriel <theledzed@gmail.com>
2018-12-14 17:55:16 -0600

    insert, concat, curtail, prepend, truncate, append

--- app/arrays.js
+++ app/arrays.js
@@ -37,27 +37,32 @@ exports.arraysAnswers = {
   },
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return(arr);
   },
 
   curtail: function(arr) {
-
+    arr.shift(arr);
+    return(arr);
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return(arr);
   },
 
   count: function(arr, item) {
