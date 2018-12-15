40fcd116b8f2b4a3599af634d28f8a6a71fd5f9b
Cristopher Uriel <theledzed@gmail.com>
2018-12-14 18:20:11 -0600

    counter: 1

--- app/count.js
+++ app/count.js
@@ -2,6 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var timer;
+    function count () {
+      console.log(start++);
+
+      if(start <= end){
+        timer = setTimeout(count, 100);
+      }
+    }
+
+    count();
 
   }
 };
