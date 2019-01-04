ceb1fde24aa2db6bd9f8358f916e927d045edf2e
Eric Allen <decoy@internetalche.me>
2019-01-04 11:07:35 -0500

    solving some more low-hanging fruit

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,37 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    // TODO: add logic for this
+    return num;
   },
 
   base10: function(str) {
-
+    // TODO: add logic for this
+    return str;
   },
 
   convertToBinary: function(num) {
-
+    // TODO: add logic for this
+    return num;
   },
 
   multiply: function(a, b) {
+    var parsedA = a.toString().split('.');
+    var parsedB = b.toString().split('.');
 
+    var aPrecision = 0;
+    var bPrecision = 0;
+
+    if (parsedA.length > 1) {
+      aPrecision = parsedA[1].length;
+    }
+
+    if (parsedB.length > 1) {
+      bPrecision = parsedB[1].length;
+    }
+
+    var precision = Math.max(aPrecision, bPrecision);
+
+    return parseFloat((a * b).toFixed(precision));
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    // TODO: add logic for this
+    return str;
   },
 
   wordWrap: function(str, cols) {
-
+    // TODO: add logic for this
+    return str;
   },
 
   reverseString: function(str) {
-
+    return str.split('').reverse().join('');
   }
 };
