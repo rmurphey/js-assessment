bffa3745b72d2fefd7a326fb9eb0424be8a632e3
Eric Allen <decoy@internetalche.me>
2019-01-05 15:07:12 -0500

    added simple module pattern solution

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    return {
+      name: str2,
+      greeting: str1,
+      sayIt: function() {
+        return this.greeting + ', ' + this.name;
+      }
+    };
   }
 };
