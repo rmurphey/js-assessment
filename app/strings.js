9dfb1d8ab4bc951e6618295d96cae6af08e329c1
tienlindavu <tienlindavu@gmail.com>
2019-01-09 16:36:25 -0500

    partial solution to strings challenge

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    var count = 0;
+    var result = '';
+    for(var i = 0; i < str.length; i++) {
+      if (str[i] === str[i+1]) {
+        count++
+        if(count < amount) {
+          result += str[i];
+        }
+      } else {
+        count = 0;
+        result += str[i];
+      }
+    };
+    return result;
   },
 
   wordWrap: function(str, cols) {
