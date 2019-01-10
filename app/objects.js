9ce1f307567cf30230cd09d7ba84898720c7ee4d
Eric Allen <decoy@internetalche.me>
2019-01-10 07:28:35 -0500

    resolved Object tests

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    return Object.keys(obj).map((key) => {
+      return `${key}: ${obj[key]}`;
+    });
   }
 };
